673460510f7c303a7789608c004e884064488a08
Heidi <heidi.e.dixon@gmail.com>
2020-02-23 12:18:07 -0800

    adding a draft template and finishing blog script

--- blog.sh
+++ blog.sh
@@ -4,7 +4,44 @@ if [ "$1" == "publish" ]; then
     git add -u
     git commit -a --allow-empty-message -m ''
     git push
-    echo blog published
+    echo "blog published"
 else
-    echo "We aren't publishing"
+    if [ "$1" == "test" ]; then
+    	bundle exec jekyll serve
+    else
+    	if [ "$1" == "newdraft" ]; then 
+    		if [ "$2" == "" ]; then
+    			echo "No file name given for new draft"
+    		else 
+				if [ -f "drafts/$2.md" ]; then
+    				echo "$2.md already exist"
+    			else 
+    				cat draft.md > drafts/$2.md
+    				git add drafts/$2.md
+    				echo "created new draft drafts/$2.md and added it to repo"
+    			fi
+    		fi
+    	else
+    		if [ "$1" == "post" ]; then
+    			if [ "$2" == "" ]; then
+    				echo "No draft name given for post"
+    			else
+    				if [ -f "drafts/$2.md" ]; then
+    					CURRENT_DATE=`date +"%Y-%m-%d"`
+    					POST_NAME="_posts/${CURRENT_DATE}-$2.html"
+    					if [ -f ${POST_NAME} ]; then 
+    						echo "post ${POST_NAME} already exists"
+    					else 
+							cat drafts/$2.md > ${POST_NAME}
+							echo "post created: ${POST_NAME}" 
+						fi
+    				else
+    					echo "No draft named drafts/$2.md found"
+    				fi
+    			fi
+    		else
+    			echo "Unknown command $1"
+    		fi
+    	fi
+    fi
 fi
---
+++ draft.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title: Draft Title
+date: 2020-01-01
+smartdown: true
+comments: false
+---
